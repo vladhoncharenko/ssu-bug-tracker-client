1724da0ad281d65984d9a526d78c49bfa95d3b1d
vladhoncharenko <vladhoncharenko@gmail.com>
2017-08-02 22:08:21 +0300

    viewPath was updated in the server.js.

--- config/server.js
+++ config/server.js
@@ -8,7 +8,7 @@ let mongoose = require('mongoose');
 let bodyParser = require('body-parser');
 let http = require('http').Server(app);
 let path = require('path');
-let viewPath = path.join(__dirname, 'app/views/');
+let viewPath = path.join(__dirname, '../app/views/');
 
 let Bug = require('../app/models/bug-schema');
 let configDB = require('../config/mongo-db-config');
@@ -22,9 +22,8 @@ mongoose.Promise = global.Promise;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('port', (process.env.PORT || 5000));
-app.use('/',express.static(path.join(__dirname, 'app/')));
-app.use('/pics',express.static(path.join(__dirname, '/pics')));
-app.use('/node_modules', express.static(__dirname + '/node_modules/angular'));
+app.use('/', express.static(path.join(__dirname, '../app/')));
+app.use('/pics', express.static(path.join(__dirname, '/pics')));
 app.set('view engine', 'ejs');
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:7777');
