531f39f7ef8ab141fc6be3c0fe82d174edc5c2a4
epismenniy <e.pismenniy@gmail.com>
2017-08-22 23:08:22 +0300

    pdf-format

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@
  */
 
 const apiRoutes = require('./api-routes');
+const PDFDocument = require('pdfkit');
 
 module.exports = function (app, viewPath, Bug, passport) {
 
@@ -38,6 +39,43 @@ module.exports = function (app, viewPath, Bug, passport) {
         req.logout();
         res.redirect('/');
     });
+
+    app.get('/bug-pdf/:id', (req, res) => {
+
+        Bug.findOne({'bugId': req.params.id}, function (err, bug) {
+
+            if (err) console.log(err);
+            //console.log(bug)
+
+            const doc = new PDFDocument()
+
+            let filename = bug.bugId;
+            filename = encodeURIComponent(filename) + '.pdf'
+
+            res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"')
+            res.setHeader('Content-type', 'application/pdf')
+
+            const date = new Date(bug.date * 1000).toISOString().slice(0, 10);
+            const building = bug.location.building;
+            const room = bug.location.room;
+            const description = bug.description;
+            const caption = bug.caption;
+
+            doc.y = 300
+            doc.font('Times-Roman');
+            doc.text('Баг №' + bug.bugId, 250, 50)
+
+            doc.text('Дата створення:  ' + date, 190, 75);
+            doc.text('Корпус:  ' + building, 250, 205);
+            doc.text('Аудиторія або інше' + room, 180, 150);
+            doc.text('Опис проблеми' + description, 215, 175);
+            doc.text(caption, 250, 200);
+
+            doc.pipe(res)
+            doc.end()
+        });
+
+    })
 
 };
 
