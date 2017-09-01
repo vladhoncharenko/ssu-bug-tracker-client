f08e186f6668794f25cefbf1941ef032cf35e280
epismenniy <e.pismenniy@gmail.com>
2017-09-01 12:50:06 +0300

    pdf was fixed

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@
 
 const apiRoutes = require('./api-routes');
 const PDFDocument = require('pdfkit');
+let bugUrl= 'ssubugtracker.com/bug/';
 
 module.exports = function (app, viewPath, Bug, passport, fs) {
 
@@ -65,17 +66,17 @@ module.exports = function (app, viewPath, Bug, passport, fs) {
 
             doc.y = 300
             doc.font('./app/content/fonts/OpenSans-Bold.ttf')
-            doc.text('Баг #' + bug.bugId, 300, 50)
+            doc.text(bugUrl + bug.bugId, 220, 50)
+
+            doc.text('Дата :  ' + date, 290, 110);
 
             let img_path = './pics/' + bug.file_name
 
-            doc.image(img_path, 25, 90, {width: 200, height:250})
+            doc.image(img_path, 50, 110, {width: 200, height:250})
 
-            doc.text('Дата створення:  ' + date, 325, 90);
-            doc.text('Корпус:  ' + building, 375, 140);
-            doc.text('Аудиторія   :   ' + room, 345, 170);
-            doc.text('Опис проблеми:   ' + description, 340, 195);
-            doc.text(caption, 375, 225);
+            doc.text('Корпус:  ' + building, 290, 140);
+            doc.text('Аудиторія   :   ' + room, 290, 170);
+            doc.text('Опис проблеми:   ' + description, 290, 200);
 
             doc.pipe(res)
             doc.end()
